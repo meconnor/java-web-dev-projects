d56db12dd038e80597cde55e01a1ed9884003ac4
Connor <c.meriweather55@gmail.com>
2023-08-21 20:10:47 -0500

    Updated print statement

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
 
-        Scanner input;
 
-        input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         double radius = input.nextDouble();
         input.close();
 
         double Area = Circle.getArea(radius);
 
-        System.out.println("Area of circle is: " + Area);
+        
+        System.out.println("Area of circle with a radius of " + radius + " is " + Area);
 
     }
 
